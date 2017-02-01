c1c0f8062f727711fbdd199944f7143bb2147df4
Simon Bee <simon.bee@autodesk.com>
2017-02-01 12:19:23 +0000

    comment

--- www/js/wgs.js
+++ www/js/wgs.js
@@ -130,17 +130,17 @@ var WGS =
 	    uniforms: {
 	        "color1": { type: "v3", value: new THREE.Vector3(41.0 / 255.0, 76.0 / 255.0, 120.0 / 255.0) },
 	        "color2": { type: "v3", value: new THREE.Vector3(1.0 / 255.0, 2.0 / 255.0, 3.0 / 255.0) },
-	        //"irradianceMap": {type: "t", value: 1.0},
-	        "envMap": { type: "t", value: null },
-	        "envRotationSin": { type: "f", value: 0.0 },
-	        "envRotationCos": { type: "f", value: 1.0 },
-	        "exposureBias": { type: "f", value: 1.0 },
-	        "envMapExposure": { type: "f", value: 1.0 },
-	        "uCamDir": { type: "v3", value: new THREE.Vector3() },
-	        "uCamUp": { type: "v3", value: new THREE.Vector3() },
-	        "uResolution": { type: "v2", value: new THREE.Vector2(600, 400) },
-	        "uHalfFovTan": { type: "f", value: 0.5 },
-	        "envMapBackground": { type: "i", value: 0 } },
+	        "irradianceMap": {type: "t", value: 1.0},
+	        //"envMap": { type: "t", value: null },
+	"exposure" : { type:"f", value: 1.0 },
+        "uCamDir": {type: "v3", value: new THREE.Vector3() },
+        "uCamUp": {type: "v3", value: new THREE.Vector3() },
+        "uResolution": {type: "v2", value: new THREE.Vector2(600, 400) },
+        "envMapBackground": {type: "i", value: 0},
+
+        "tBackground": {type: "t", value: null},
+        "bUseBackground": {type: "i", value: 0}
+ },
 
 	    
 	    vertexShader: __webpack_require__(2),
